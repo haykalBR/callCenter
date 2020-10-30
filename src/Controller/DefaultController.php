166fda5665a053d2ea63e9d0a68058f1d9509f7e
haykalBR <haikelbrinis@gmail.com>
2020-10-31 00:53:20 +0100

    a

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends AbstractController
 
             $user=$repository->find(3268);
             $logs = $repo->getLogEntries($user);
-            $repo->revert($user, 4/*version*/);
+            $repo->revert($user, 2/*version*/);
             dd($user->getEmail());
 
             $user->setEmail('ssss@mo3a9.com');
